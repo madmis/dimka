d66c701096c8390b599acc4846db99cd70d4a87b
Dmitry Machin <dmitry.machin@timeout.com>
2018-06-14 14:14:30 +0300

    Fix travis config

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+    name="dimka",
+    version="0.0.1-betta",
+    license='MIT',
+    author="Machin Dmytro",
+    author_email="machin.dmitry+pypi.org@gmail.com",
+    description="Wex.nz Trading Bot",
+    long_description="Wex.nz Trading Bot",
+    long_description_content_type="text/markdown",
+    url="https://github.com/madmis/dimka",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
